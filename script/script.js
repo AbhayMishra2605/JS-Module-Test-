21cb81b7096a279ae021b5beec88be1be288e72b
AbhayMishra2605 <152012187+AbhayMishra2605@users.noreply.github.com>
2024-09-24 09:46:25 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,121 @@
+let com_score;
+let user_score;
+
+fetch('score.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('template-container').innerHTML = data;
+
+    com_score=document.getElementById("computerScore");
+    user_score=document.getElementById("userScore");
+
+    const stroredComScore =localStorage.getItem('computerScore') || 0;
+    const stroredUserScore = localStorage.getItem('userScore') || 0;
+
+    com_score.value = stroredComScore;
+    user_score.value = stroredUserScore;
+})
+.catch(error => console.log('Error:', error));
+
+
+function game(userChoice) {
+    const choices = ['rock', 'paper', 'scissor'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    
+
+    if (userChoice === 'scissor') {
+  if (computerChoice === 'paper') {
+        
+        user_score.value = parseInt(user_score.value) + 1;
+        localStorage.setItem('userScore', user_score.value);
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "win.html";
+            
+
+        
+
+    } else if (computerChoice === 'rock') {
+        com_score.value = parseInt(com_score.value) + 1;
+        localStorage.setItem('computerScore', com_score.value);
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "loss.html";
+    } else {
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "tie.html";
+    }
+} else if (userChoice === 'paper') {
+    if (computerChoice === 'rock') {
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "win.html";
+        user_score.value = parseInt(user_score.value) + 1;
+        localStorage.setItem('userScore',user_score.value);
+        
+    } else if (computerChoice === 'scissor') {
+        com_score.value = parseInt(com_score.value) + 1;
+        localStorage.setItem('computerScore',com_score.value);
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "loss.html";
+    } else {
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "tie.html";
+    }
+} else if (userChoice==='rock'){
+    if (computerChoice === 'scissor') {
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "win.html";
+        user_score.value = parseInt(user_score.value) + 1;
+        localStorage.setItem('userScore', user_score.value);
+    } else if (computerChoice === 'paper'){
+        com_score.value = parseInt(com_score.value) + 1;
+        localStorage.setItem('computerScore', com_score.value);
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "loss.html";
+    }else {
+        localStorage.setItem('userpick',userChoice);
+        localStorage.setItem('comppick',computerChoice);
+        window.location.href = "tie.html";
+    }
+}
+
+}
+
+fetch('Rule.html')
+.then(response=> response.text())
+.then(data=>{
+    document.getElementById('rule-container').innerHTML = data;
+})
+
+
+
+
+function openRule(){
+    let closeButton = document.getElementById("rule-container");
+    closeButton.style.display='block';
+}
+
+
+function closeFun(){
+    let closeButton = document.getElementById("rule-container");
+    closeButton.style.display='none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script/script1.js
@@ -0,0 +1,59 @@
+let userPick = localStorage.getItem('userpick');
+let comPick = localStorage.getItem('comppick');
+
+let imgElement = document.querySelector("#youPiked img");
+let imgElement1 = document.querySelector("#pcPiked img");
+let tag =document.querySelector("#youPiked #wrockSec");
+let tag1 =document.querySelector("#pcPiked div");
+
+    if (userPick === 'rock') {
+        imgElement.src = "Pictures/icons8-fist-67 1.png"; 
+        imgElement.id = "rockImg";  
+        tag.style.borderWidth = "0.8vw";
+        tag.style.borderStyle = "solid";
+        tag.style.borderColor = "#0074B6"; 
+    } else if (userPick === 'paper') {
+        imgElement.src = "Pictures/icons8-hand-64 1.png";
+        imgElement.id = "paperImg";  
+        tag.style.borderWidth = "0.8vw";
+        tag.style.borderStyle = "solid";
+        tag.style.borderColor = "#FFA943"; 
+    } else if (userPick === 'scissor') {
+        imgElement.src = "Pictures/sessior.png";
+        imgElement.id = "sessiorImg";  
+        tag.style.borderWidth = "0.8vw";
+        tag.style.borderStyle = "solid";
+        tag.style.borderColor = "#BD00FF";
+        
+        
+    }
+    if (comPick === 'rock') {
+        imgElement1.src = "Pictures/icons8-fist-67 1.png"; 
+        imgElement1.id = "rockImg";  
+        tag1.style.borderWidth = "0.8vw";
+        tag1.style.borderStyle = "solid";
+        tag1.style.borderColor = "#0074B6"; 
+    } else if (comPick === 'paper') {
+        imgElement1.src = "Pictures/icons8-hand-64 1.png";
+        imgElement1.id = "paperImg";  
+        tag1.style.borderWidth = "0.8vw";
+        tag1.style.borderStyle = "solid";
+        tag1.style.borderColor = "#FFA943"; 
+    } else if (comPick === 'scissor') {
+        imgElement1.src = "Pictures/sessior.png";
+        imgElement1.id = "sessiorImg";  
+        tag1.style.borderWidth = "0.8vw";
+        tag1.style.borderStyle = "solid";
+        tag1.style.borderColor = "#BD00FF";
+        
+        
+    }
+    
+   
+
+    setTimeout(()=>{
+        let massage=document.querySelector('.massage');
+        massage.style.display='block';
+       
+
+    },600);
